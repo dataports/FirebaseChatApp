c1be4b67e57ee5de30ac19ead80057c57973229c
Sophie <5545720+dataports@users.noreply.github.com>
2018-05-17 17:23:31 -0600

    Fixed gradle issues and dependencies

--- app/src/main/java/com/example/a774261/firebasechatapp/MainActivity.java
+++ app/src/main/java/com/example/a774261/firebasechatapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.a774261.firebasechatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+//Firebase
+
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
-import android.widget.Toast;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
+
+    private static final int RC_SIGN_IN = 123;
+
+// ...
+
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build());
+//            new AuthUI.IdpConfig.PhoneBuilder().build(),
+//            new AuthUI.IdpConfig.GoogleBuilder().build(),
+//            new AuthUI.IdpConfig.FacebookBuilder().build(),
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +38,12 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         if(FirebaseAuth.getInstance().getCurrentUser() == null) {
             // Start sign in/sign up activity
-//            startActivityForResult(
-//                    AuthUI.getInstance()
-//                            .createSignInIntentBuilder()
-//                            .build(),
-//                    SIGN_IN_REQUEST_CODE
-//            );
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    RC_SIGN_IN
+            );
         } else {
             // User is already signed in. Therefore, display
             // a welcome Toast
