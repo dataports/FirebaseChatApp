963e5ec693fe4c4bc09213e17088fd4613f9b514
Sophia Amin <774261@ACDM.DS.SAIT.CA>
2018-06-24 17:09:44 -0600

    Camera Activity Fragment added

--- app/src/main/java/com/example/a774261/firebasechatapp/MainActivity.java
+++ app/src/main/java/com/example/a774261/firebasechatapp/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -141,6 +142,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //camera button functions
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         rootRef = FirebaseDatabase.getInstance().getReference();
         Intent intent = new Intent(this, MainActivity.class);
